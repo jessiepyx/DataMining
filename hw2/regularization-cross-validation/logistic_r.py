fix bug for validation: normalization, ridge and logistic

hw2/regularization-cross-validation/logistic_r.py
@@ -21,7 +21,7 @@ def logistic_r(X, y, lmbda):
 
     min_loss = 0
     max_iters = 1000
-    learning_rate = 10
+    learning_rate = 0.01
 
     iter = 0
     h = 1 / (1 + np.exp(-w.T.dot(X_with_b)))  # sigmoid
@@ -35,10 +35,11 @@ def logistic_r(X, y, lmbda):
 
         # update
         iter += 1
-        h = 1 / (1 + np.exp(-w.T.dot(X_with_b)))
+        h = 1. / (1 + np.exp(-w.T.dot(X_with_b)))
+        
         loss = - y.dot(np.log(h).T) + (1 - y).dot(np.log(1 - h).T) / N
         loss += lmbda * w.T.dot(w) / 2 / N
 
     # end answer
-    
+
     return w

hw2/regularization-cross-validation/ridge.py
@@ -19,8 +19,15 @@ def ridge(X, y, lmbda):
     # begin answer
 
     X_with_b = np.vstack((np.ones((1, N)), X))
+    
+    mat = np.matmul(X_with_b, X_with_b.T) + lmbda * np.eye(P + 1)
+    
+    if lmbda == 0:
+        w = np.linalg.pinv(mat)
+    else:
+        w = np.linalg.inv(mat)
 
-    w = np.linalg.pinv(np.matmul(X_with_b, X_with_b.T) + lmbda * np.eye(P + 1)).dot(X_with_b).dot(y.T)
+    w = w.dot(X_with_b).dot(y.T)
 
     # end answer
     return w